6dabc68dcbe0b905efa41b2ec34ac390d3f0362d
CallumI <callum.iddon@gmail.com>
2017-08-22 21:43:23 +0100

    Add constant move vectors

--- logic.js
+++ logic.js
@@ -2,7 +2,8 @@
 
 /* This file deals with game logic... where can a piece move? */
 // Castling is treated as a move for the king
-
+const max = Math.max;
+const min = Math.min;
 
 function getPossibleNewStates(oldState){
   for(let i = 0 ; i < BOARD_SIZE; i++){
@@ -15,12 +16,36 @@ function getPossibleNewStates(oldState){
   }
 }
 
+/* The moves are a list of lists of vectors. Each inner list contains
+ * all moves in a given direction... You can stop looking at the later
+ * vectors when one impossible one is reached and move to the next inner list.
+ * eg: Rook contains four lists: up, left, down, right.
+ * Pawn is not given because it is colour specific and complex.
+ */
 
-function getNewStatesPawn(oldState, file, rank){
-  const isWhite = isWhiteToPlay(oldState);
-  if(isWhite){
-    if(rank == 2){
+const kingAllMoves = [
+  [[-1,  1]], [[0,  1]], [[1,  1]],
+  [[-1,  0]],            [[1,  0]],
+  [[-1, -1]], [[0, -1]], [[1, -1]]
+];
 
-    }
-  }
-}
+const rookAllMoves = [
+  [[-1,  0], [-2,  0], [-3,  0], [-4, 0], [-5, 0], [-6, 0], [-7, 0]],
+  [[ 1,  0], [ 2,  0], [ 3,  0], [ 4, 0], [5, 0], [6, 0], [7, 0]],
+  [[ 0, -1], [ 0, -2], [ 0, -3], [ 0, -4], [0, -5], [0, -6], [0, -7]],
+  [[ 0,  1], [ 0,  2], [ 0,  3], [ 0,  4], [0,  5], [0,  6], [0,  7]]
+];
+
+const bishopAllMoves = [
+  [[-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7]],
+  [[ 1, -1], [ 2, -2], [ 3, -3], [ 4, -4], [ 5, -5], [ 6, -6], [ 7, -7]],
+  [[-1,  1], [-2,  2], [-3,  3], [-4,  4], [-5,  5], [-6,  6], [-7,  7]],
+  [[ 1,  1], [ 2,  2], [ 3,  3], [ 4,  4], [ 5,  5], [ 6,  6], [ 7,  7]]
+];
+
+const knightAllMoves = [
+  [[1, 2]], [[-1,  2]], [[1, -2]], [[-1, -2]],
+  [[2, 1]], [[ 2, -1]], [[-2, 1]], [[-2, -1]]
+];
+
+const queenAllMoves = Array.prototype.concat(knightAllMoves, bishopAllMoves);
